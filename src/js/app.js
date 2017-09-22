7068c704a9e87a3ea646237685520d359406d06f
logic0verflow <logic0verflow90@gmail.com>
2017-09-22 11:18:06 -0600

    Refine menu, add a few apts to map and viewmodel

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,79 @@
+
 var map;
+
+var viewModel;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8,
+        center: {lat: 37.335216, lng: -121.887814},
+        zoom: 12    ,
         fullscreenControl: false,
-        mapTypeControlOptions: {
-            position: google.maps.ControlPosition.TOP_RIGHT
-        }
+        mapTypeControl: false
     });
+
+
+    var service = new google.maps.places.PlacesService(map);
+    var request = {
+        location: {lat: 37.335216, lng: -121.887814},
+        radius: 10000,
+        keyword: "apartment"
+    };
+
+    infoWindow = new google.maps.InfoWindow();
+
+    service.nearbySearch(request, function(result) {
+
+        viewModel = { apartment: result };
+        ko.applyBindings(viewModel);
+
+        viewModel.apartment.forEach(function(apt) {
+            var marker = new google.maps.Marker({
+                position: apt.geometry.location,
+                map: map,
+                title: apt.name
+            });
+            marker.addListener('click', function() {
+                infoWindow.close();
+                var content = '<div id="info-window"><h3>'+apt.name+'</h3></div>';
+                infoWindow.setContent(content);
+                infoWindow.open(map,this);
+            });
+
+        });
+
+    });
+
 }
 
+var infoWindow;
+
+
+
+
+
+
 var menuOpen = false;
+
 function toggleMenu() {
+
+    // Keep the menu at a max width
+    var maxWidth = 600;
+    var width = $(window).width();
+    console.log(width);
+    width = (width > maxWidth) ? maxWidth : width;
+    menuWidth = width * 0.9;
+    menuWidth += "px";
+    $("#main-menu").css("width", menuWidth);
+
     if (menuOpen) {
         menuOpen = false;
-        document.getElementById("main-menu").style.left = "-500px";
+        document.getElementById("main-menu").style.left = "-" + menuWidth;
+        document.getElementById("menu-toggle").style.left = "0";
         document.getElementById("map").style.marginLeft = "0";
     } else {
         menuOpen = true;
         document.getElementById("main-menu").style.left = "0";
-        document.getElementById("map").style.marginLeft = "500px";
+        document.getElementById("menu-toggle").style.left = menuWidth;
+        document.getElementById("map").style.marginLeft = menuWidth;
     }
 }
