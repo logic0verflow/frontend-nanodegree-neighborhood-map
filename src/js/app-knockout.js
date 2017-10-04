534306479bb1ca2c956dcc4fb0954b74e2ef8512
logic0verflow <logic0verflow90@gmail.com>
2017-10-04 09:35:46 -0600

    Update: Setup pane switching in main menu drawer

--- src/js/app-knockout.js
+++ src/js/app-knockout.js
@@ -1,4 +1,4 @@
-// Knockout ViewModel that stores all the info used by the main menu
+    // Knockout ViewModel that stores all the info used by the main menu
 var ViewModel = function() {
     var self = this;
 
@@ -22,8 +22,75 @@ var ViewModel = function() {
     this.showMovies = ko.observable(true);
     this.showParks = ko.observable(true);
 
+    this.selectedListing = {
+        name: ko.observable("DEFAULT NAME"),
+        // description: ko.observable("DEFAULT DESCRIPTION"),
+        address: ko.observable("DEFAULT ADDRESS"),
+        hours: ko.observable("DEFAULT HOURS"),
+        phone: ko.observable("DEFAULT PHONE"),
+        website: ko.observable("DEFAULT WEBSITE")
+    };
+
     // All the markers that get created on the Google Map
     this.markers = {};
+
+    // Used to keep state of which pane is showing
+    this.showingPlaceInfo = ko.observable(false);
+    this.showingListings = ko.observable(true);
+    this.showingCredits = ko.observable(false);
+
+    self.showPlaceInfo = function(place) {
+        // hide all panes except place info
+        self.showingPlaceInfo(true);
+        self.showingListings(false);
+        self.showingCredits(false);
+
+        var request = { placeId: place.place_id };
+
+        var service = new google.maps.places.PlacesService(map);
+        service.getDetails(request, function(place, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                self.selectedListing.name(place.name);
+                self.selectedListing.address(place.formatted_address);
+                self.selectedListing.phone(place.formatted_phone_number);
+                self.selectedListing.website(place.website);
+            }
+        });
+
+    };
+
+    self.showListings = function(place) {
+        // switch panes from listings to location info pane
+        self.showingCredits(false);
+        self.showingPlaceInfo(false);
+        self.showingListings(true);
+
+        // self.selectedListing.name(place.name);
+    };
+
+    self.showCredits = function() {
+        self.showingCredits(!self.showingCredits());
+        self.showingPlaceInfo(false);
+        self.showingListings(false);
+    }
+};
+
+// Custom knockout binding to fade elements in or out
+ko.bindingHandlers.fadeVisible = {
+    // Start visible/invisible according to initial value
+    init: function(element, valueAccessor) {
+        var visible = valueAccessor();
+        // visible is a knockout observable, need to get value using ()
+        visible = visible();
+        $(element).toggle(visible);
+    },
+    // On update, fade in/out
+    update: function(element, valueAccessor) {
+        var visible = valueAccessor();
+        // visible is a knockout observable, need to get value using ()
+        visible = visible();
+        visible ? $(element).fadeIn() : $(element).fadeOut();
+    }
 };
 
 var viewModel = new ViewModel();
